a6b707aae38cc910b22572aaf30d10e4b5c93a6f
suryapranesh_300068342 <surya.pranesh@myntra.com>
2020-06-04 21:54:31 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+// app.use(urlencoded({extended: false}));
+// app.use(json());
+
+app.get('/', (req,res) => {
+    res.send("Hi Swee");
+})
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,()=>{
+    console.log("Listening on PORT:",PORT);
+})
